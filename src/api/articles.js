d1fd36f37d04febfd9add496bb3f64f519aac608
zhangjiajia <756661181@qq.com>
2019-09-07 13:53:37 +0800

    加载文章列表1

--- src/api/articles.js
+++ src/api/articles.js
@@ -2,21 +2,21 @@ import request from '@/utils/request'
 
 export const getArticles = ({
   // 频道id
-  channel_id: channelId, // 格式不支持 '_' == 需要赋值才可操作
+  channelId,
   // 当前时间戳
   timestamp,
   // 是否包含置顶 - 1置顶 ，0不包含置顶
-  with_top: withTop // 格式不支持 '_' == 需要赋值才可操作
+  withTop
 }) => {
   return request.get('/app/v1_1/articles', {
     // 返回值是Query | params对象
     params: {
       // 频道id
-      channelId,
+      channel_id: channelId, // 格式不支持 '_' == 需要赋值才可操作
       // 当前时间戳
       timestamp,
       // 是否包含置顶 - 1置顶 ，0不包含置顶
-      withTop
+      with_top: withTop// 格式不支持 '_' == 需要赋值才可操作
     }
   })
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,12 +4,15 @@
     <van-nav-bar fixed title="首页" />
     <!-- 频道列表 -->
     <van-tabs animated v-model="activeIndex">
+      <!--单击按钮 弹出 频道弹层  -->
+      <!-- <van-icon slot="nav-right" name="wap-nav" @click="showChannelsEdit=true"/> -->
       <!-- 频道标签 -->
       <van-tab v-for="channel in channels" :title="channel.name" :key="channel.id">
         <!-- 上拉刷新事件 -->
         <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
           <!-- 文章列表,不同的标签页下有不同的列表 -->
-          <van-cell v-for="item in list" :key="item" :title="item" />
+          <van-cell v-for="article in currentChannel.articles" :key="article.art_id.toString()" :title="article.title" >
+          </van-cell>
         </van-list>
       </van-tab>
     </van-tabs>
@@ -18,11 +21,12 @@
 
 <script>
 import { getUserChannels } from '@/api/channel'
+import { getArticles } from '@/api/articles'
 
 export default {
   data () {
     return {
-      // 设置tabs默认索引
+      // 设置tabs默认索引,通过索引找当前频道
       activeIndex: 0,
       // 列表
       list: [],
@@ -37,6 +41,12 @@ export default {
   created () {
     this.getChannelsList()
   },
+  computed: {
+    // 返回当前频道对象
+    currentChannel () {
+      return this.channels[this.activeIndex]
+    }
+  },
   methods: {
     // 获取文章列表 ---------------------------------------------
 
@@ -44,26 +54,40 @@ export default {
     async getChannelsList () {
       try {
         const data = await getUserChannels()
+        // 给所有的列表设置时间戳(每次上拉需要) 文章数组(上拉更新后需要push进之前的列表)
+        // forEach 遍历给每个频道都加入
+        data.channels.forEach((channel) => {
+          // 当前时间戳 设空(第一次获取没有当前时间戳)
+          channel.timestamp = null
+          // 文章列表
+          channel.articles = []
+        })
+        // 将获取的频道赋值给 data()中绑定的channels[循环列表]
         this.channels = data.channels
-        // console.log(this.channels)
-      } catch (err) {
-        console.log(err)
+      } catch (error) {
+        console.log(error)
       }
     },
     // 上拉事件 -------------------------------------------------
-    onLoad () {
-      // 异步更新数据
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1)
-        }
-        // 加载状态结束
-        this.loading = false
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true
-        }
-      }, 500)
+    async onLoad () {
+      // 发送请求 - 获取频道列表
+      // 获取当前频道对象 - 因是变动值(每次上拉更新)需设计算属性
+      // computed 中
+      const data = await getArticles({
+        // 获取参数 id 时间戳 是否置顶 === 赋值
+        // id : 计算属性[获取上拉时当前属于哪个频道的id]
+        channelId: this.currentChannel.id,
+        // 时间戳 : 前一页历史数据 如第一次获取列表赋值最新时间
+        timestamp: this.currentChannel.timestamp || Date.now(),
+        // // 是否包含置顶 - 1置顶 ，0不包含置顶
+        withTop: 1
+      })
+      // 获取最后一条列表的时间戳 赋值 文章列表当前时间戳
+      this.currentChannel.timestamp = data.pre_timestamp
+      // 将获取到的文章push添加到之前列表中
+      this.currentChannel.articles.push(...data.results)
+      // 加载完成后 loading关闭
+      this.loading = false
     }
   }
 }
